07a1bc73ea8b7ded2feb54fea6f01818f5b4a3da
AN-Fahmy <fahmyan@taibaconnect.com>
2026-06-10 16:32:00 +0300

    change song

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,13 +1,11 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { TranslatePipe } from '@ngx-translate/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [TranslatePipe],
+  imports: [],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss',
-  schemas:[CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AboutComponent {
   scrollToSection(sectionId: string) {
