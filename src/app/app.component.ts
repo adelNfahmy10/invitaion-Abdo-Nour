411416572ae86ba6a2dfb7f6312ab7296045a187
AN-Fahmy <fahmyan@taibaconnect.com>
2026-06-10 17:39:09 +0300

    add new song

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA, HostListener, inject, PLATFORM_ID } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, ElementRef, HostListener, inject, OnInit, PLATFORM_ID, ViewChild } from '@angular/core';
 import { isPlatformBrowser, NgClass } from '@angular/common';
 import { NgwWowService } from 'ngx-wow';
 import { HomeComponent } from "./home/home.component";
@@ -15,15 +15,18 @@ import { DateComponent } from "./date/date.component";
   styleUrl: './app.component.scss',
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   private readonly _PLATFORM_ID = inject(PLATFORM_ID)
   private readonly _NgwWowService = inject(NgwWowService)
 
   ngOnInit(): void {
     if (isPlatformBrowser(this._PLATFORM_ID)) {
-      this._NgwWowService.init()
+      this._NgwWowService.init();
     }
-    this.generateHearts();
+
+    setTimeout(() => {
+      this.palySound()
+    }, 500);
   }
 
   showArrow:boolean = false
@@ -40,9 +43,7 @@ export class AppComponent {
     window.scrollTo({ top: 0, behavior: 'smooth' });
   }
 
-
   hearts: any[] = [];
-
   generateHearts() {
     this.hearts = Array.from({ length: 40 }).map(() => ({
       left: Math.random() * 100,
@@ -50,5 +51,26 @@ export class AppComponent {
       delay: Math.random() * 10,
       size: 6 + Math.random() * 14,
     }));
+  }
+
+  audio!: HTMLAudioElement;
+
+  palySound() {
+    if (isPlatformBrowser(this._PLATFORM_ID)) {
+      this.audio = new Audio('assets/audio/song.mp3');
+      this.audio.loop = true;
+
+      const start = () => {
+        this.audio.play().catch(console.error);
+
+        document.removeEventListener('click', start);
+        document.removeEventListener('touchstart', start);
+        document.removeEventListener('scroll', start);
+      };
+
+      document.addEventListener('click', start, { once: true });
+      document.addEventListener('touchstart', start, { once: true });
+      document.addEventListener('scroll', start, { once: true, passive: true });
+    }
   }
 }
